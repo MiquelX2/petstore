413b58e251a7eafdcaa0478a56d8b89218b60cd0
Student <student@debian>
2017-04-24 12:49:32 +0200

    events

--- src/pl/pcz/client/petstore.java
+++ src/pl/pcz/client/petstore.java
@@ -1,6 +1,9 @@
 package pl.pcz.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -9,20 +12,52 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class petstore implements EntryPoint {
+public class petstore implements EntryPoint, Scheduler.RepeatingCommand {
 
+    private Label header = new Label();
     private FlexTable table = new FlexTable();
     private Label id = new Label("123");
     private Label name = new Label("Spot");
     private Label category = new Label("reserved");
+    private int i = 0;
 
+    public boolean execute() {
+	header.setText("Petstore: " + i);
+	++i;
+	return true;
+    }
+    
+    private void addPet(final String id, final String name, final String category) {
+
+	ClickHandler h = new ClickHandler() {
+	        String m_id = id;
+		String m_name = name;
+		String m_category = category;
+
+		public void onClick(ClickEvent event) {
+		    petstore.this.id.setText(m_id);
+		    petstore.this.name.setText(m_name);
+		    petstore.this.category.setText(m_category);
+		}
+	    };
+	    
+	int row = table.getRowCount();
+	Label l1 = new Label(id);
+	Label l2 = new Label(name);
+	l1.addClickHandler(h);
+	l2.addClickHandler(h);
+	table.setWidget(row, 0, l1);
+	table.setWidget(row, 1, l2);
+    }
+    
     public void onModuleLoad() {
+	Scheduler.get().scheduleFixedPeriod(this, 1000);
+	
 	// Sample pet data.
 	table.addStyleName("table");
-	table.setText(0, 0, "123");
-	table.setText(0, 1, "Spot");
+	addPet("123", "Spot", "available");
+	addPet("321", "Duff", "taken");
 
-        Label header = new Label("Petstore");
 	header.addStyleName("header");
 
 	VerticalPanel right = new VerticalPanel();
